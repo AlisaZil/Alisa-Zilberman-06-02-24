f2eee62b3bf2f5bda71a687ded99e5ab6a8f1cfd
AlisaZil <alisazilberman@gmail.com>
2024-02-07 05:04:37 -0800

    merge main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { MainComponent } from './main/main.component';
-=======
 import { WeatherCardComponent } from './weather-card/weather-card.component';
->>>>>>> main
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    MainComponent
-=======
+    MainComponent,
     WeatherCardComponent
->>>>>>> main
   ],
   imports: [
     BrowserModule,
