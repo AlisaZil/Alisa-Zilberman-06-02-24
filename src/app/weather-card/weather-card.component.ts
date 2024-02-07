92840ea0156f2e737fe9bb3241de53336de554f4
AlisaZil <alisazilberman@gmail.com>
2024-02-07 04:39:08 -0800

    add(card input vars)

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: ['./weather-card.component.scss']
 })
+  
 export class WeatherCardComponent {
+
+  @Input() card?: { placeName: string, icon: string, temp: string | number };
 
 }
