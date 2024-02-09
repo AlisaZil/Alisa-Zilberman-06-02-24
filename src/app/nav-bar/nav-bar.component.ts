b0e7e66596660338102d64e554deeef965089219
AlisaZil <alisazilberman@gmail.com>
2024-02-09 03:35:55 -0800

    add(toggle output for navbar)

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,8 +8,13 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class NavBarComponent {
 
   @Output() buttonLinkClick = new EventEmitter<MouseEvent>();
+  @Output() toggleLinkClick = new EventEmitter<{value:boolean, name:string}>();
 
   handleButtonLinkClick(e:MouseEvent) {
     this.buttonLinkClick.emit(e);
+  }
+
+  handleToggleClick(value: boolean, name: string) {
+    this.toggleLinkClick.emit({ value: value, name: name });
   }
 }
