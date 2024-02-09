6bb2676a66b028bd6ba036362094296b30c4f3c6
AlisaZil <alisazilberman@gmail.com>
2024-02-09 01:59:14 -0800

    add(nav-bar component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherCardComponent
+    WeatherCardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
