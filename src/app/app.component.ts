55fe26ddc91d0b3e5c8445605c785dfcd6a31794
AlisaZil <alisazilberman@gmail.com>
2024-02-10 06:08:45 -0800

    add(animation to search bar )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { WeatherService } from './services/weather.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,34 +7,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-
-  placesList: any[] = [];
-  isComboBoxOpen: boolean = false;
 
   public isNavbarOpen: boolean = false;
 
-  constructor(private weatherService: WeatherService,
-  private router: Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void { }
-  
-   searchForPlaces(e: KeyboardEvent) {
-    
-    let inputValue: string = (e.target as HTMLInputElement).value;
-
-    if (inputValue) {
-
-      this.isComboBoxOpen = true;
-
-      // this.weatherService.getPlacesByString(inputValue).subscribe(res => {
-      //   this.placesList = res;
-      // })
-    } else {
-      this.placesList = [];
-      this.isComboBoxOpen = false;
-    }
-  }
 
   handleButtonLinkClick(e: string) {
     this.isNavbarOpen = false;
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
-  selector: 'app-search-bar',
+  selector: 'search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.sass']
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent {
 
+  @Output() selectedPlace = new EventEmitter<{ name: string, key: string }>();
+
+  placesList: any[] = ['tokyo','tel aviv', 'london','monte carlo'];
+  isComboBoxOpen: boolean = false;
+
+  constructor(private weatherService: WeatherService,){}
+
+    searchForPlaces(e: KeyboardEvent) {
+    
+    let inputValue: string = (e.target as HTMLInputElement).value;
+
+    if (inputValue) {
+
+      this.isComboBoxOpen = true;
+
+      this.placesList = ['tokyo', 'tel aviv', 'london', 'monte carlo'];
+      console.log(this.placesList);
+      
+      // this.weatherService.getPlacesByString(inputValue).subscribe(res => {
+      //   this.placesList = res;
+      //   console.log(res);
+        
+      // })
+
+    } else {
+      this.placesList = [];
+      this.isComboBoxOpen = false;
+    }
+  }
 }
