db7fd44b39f5006f82e06f73660e42ea9c4c080b
AlisaZil <alisazilberman@gmail.com>
2024-02-11 09:58:11 -0800

    add(pipe)

---
+++ src/app/c-tof.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CTofPipe } from './c-tof.pipe';
+
+describe('CTofPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CTofPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
